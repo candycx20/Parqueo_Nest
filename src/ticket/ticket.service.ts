aefba81b48056cb977176c9b4828da0b27678d17
candycx20 <candycarrillo060@gmail.com>
2024-05-05 19:12:38 -0600

    Respusta de estado

--- src/ticket/ticket.service.ts
+++ src/ticket/ticket.service.ts
@@ -22,10 +22,24 @@ export class TicketService {
 
     async createTicket(data) {
         try {
+
+            const ticketData = {
+                ...data,
+                estado: 0
+            };
+
             const result = await this.prismaService.tickets.create({
-                data
+                data: ticketData
             });
+
             const response = await lastValueFrom(this.httpService.post('http://localhost:8000/tickets/', result));
+            const nuevoEstado = response.data.estado;
+
+            await this.prismaService.tickets.update({
+                where: { id: result.id },
+                data: { estado: nuevoEstado }
+            });
+
             return result;
         } catch (error) {
             console.log(error);
